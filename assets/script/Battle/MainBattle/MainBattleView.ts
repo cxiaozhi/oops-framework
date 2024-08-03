e05ca8a192bcdffb564b8bc0201334394b1a06e6
Ccz <2047638475@qq.com>
2024-08-03 19:14:31 +0800

    update

---
+++ assets/script/Battle/MainBattle/MainBattleView.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('MainBattleView')
+export class MainBattleView extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
---
+++ update-oops-plugin-all.bat
@@ -0,0 +1,9 @@
+start "1" cmd /k call update-oops-plugin-bundle.bat
+
+start "2" cmd /k call update-oops-plugin-excel-to-json.bat
+
+start "3" cmd /k call update-oops-plugin-framework.bat
+
+start "4" cmd /k call update-oops-plugin-framework-tools.bat
+
+start "5" cmd /k call update-oops-plugin-hot-update.bat
