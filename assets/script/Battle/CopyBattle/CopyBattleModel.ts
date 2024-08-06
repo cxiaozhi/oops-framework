02a0c7bdc9b9762370fbb7842c772f97b47d254b
ccz <2047638475@qq.com>
2024-08-06 11:49:19 +0800

    feat: 🎸 update

---
+++ assets/script/Battle/CopyBattle/CopyBattleModel.ts
@@ -0,0 +1,12 @@
+import { _decorator, Component, Node } from "cc";
+const { ccclass, property } = _decorator;
+
+/**
+ * 副本
+ */
+@ccclass("CopyBattlModel")
+export class CopyBattlModel extends Component {
+    start() {}
+
+    update(deltaTime: number) {}
+}
